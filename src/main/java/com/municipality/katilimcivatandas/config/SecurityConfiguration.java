4ff5d6a49cc63c5f6e48757d0206b2482899f7a5
Burak <bahmetpalanci@gmail.com>
2019-12-08 15:46:04 +0300

    Add swagger ui

--- src/main/java/com/municipality/katilimcivatandas/config/SecurityConfiguration.java
+++ src/main/java/com/municipality/katilimcivatandas/config/SecurityConfiguration.java
@@ -69,6 +69,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.OPTIONS, "/**")
             .antMatchers("/h2-console/**")
             .antMatchers("/swagger-ui/index.html")
+            .antMatchers("/swagger-ui.html")
             .antMatchers("/test/**");
     }
 
